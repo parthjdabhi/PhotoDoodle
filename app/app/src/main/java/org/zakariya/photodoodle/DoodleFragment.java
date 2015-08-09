d05a31a0b0633f03b0c6b4d92b70677066fe8339
Shamyl Zakariya <shamyl@gmail.com>
2015-08-09 15:12:07 -0700

    Bring in IcePick for state serialization

--- app/app/src/main/java/org/zakariya/photodoodle/DoodleFragment.java
+++ app/app/src/main/java/org/zakariya/photodoodle/DoodleFragment.java
@@ -9,19 +9,34 @@ import android.view.ViewGroup;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import icepick.Icepick;
 
 /**
  * Created by shamyl on 8/9/15.
  */
 public class DoodleFragment extends Fragment {
 
-	@Bind(R.id.doodleView) DoodleView doodleView;
+	@Bind(R.id.doodleView)
+	DoodleView doodleView;
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Icepick.restoreInstanceState(this, savedInstanceState);
+	}
 
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_doodle,container,false);
-		ButterKnife.bind(this,v);
+		View v = inflater.inflate(R.layout.fragment_doodle, container, false);
+		ButterKnife.bind(this, v);
 		return v;
 	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		Icepick.saveInstanceState(this, outState);
+	}
+
 }

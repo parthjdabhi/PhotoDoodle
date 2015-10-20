aa3cec1ad75389b9eb71e116005d5d96c6a38ef2
Shamyl Zakariya <shamyl@gmail.com>
2015-10-20 08:03:27 -0700

    Move line intersection code into LineSegment, and delete Lines.java

--- app/app/src/main/java/org/zakariya/photodoodle/geom/LineSegment.java
+++ app/app/src/main/java/org/zakariya/photodoodle/geom/LineSegment.java
@@ -1,6 +1,7 @@
 package org.zakariya.photodoodle.geom;
 
 import android.graphics.PointF;
+import android.support.annotation.Nullable;
 import android.util.Pair;
 
 /**
@@ -72,5 +73,42 @@ public class LineSegment {
 		// compute distance from point to the closest projection point on the line segment
 		PointF projectedOnSegment = PointFUtil.add(a, PointFUtil.scale(dir, projectedLength));
 		return PointFUtil.distance2(point, projectedOnSegment);
+	}
+
+	/**
+	 * Find the intersection point of this LineSegment and another, if any.
+	 * @param other the line segment to intersect
+	 * @param bounded if false, the line segments are treated as infinite-length lines
+	 * @return the intersection point, if any. null if the lines are parallel, or if the test is bounded and the segments do not intersect
+	 */
+	@Nullable
+	public PointF intersection(LineSegment other, boolean bounded) {
+		// cribbed from: http://wiki.processing.org/w/Line-Line_intersection
+		float adx = other.a.x - a.x;
+		float ady = other.a.y - a.y;
+		float bdx = other.b.x - b.x;
+		float bdy = other.b.y - b.y;
+		float ad_dot_bd = adx * bdy - ady * bdx;
+
+		if (Math.abs(ad_dot_bd) < 1e-4) {
+			return null;
+		}
+
+		float abx = b.x - a.x;
+		float aby = b.y - a.y;
+		float t = (abx * bdy - aby * bdx) / ad_dot_bd;
+
+		if (bounded) {
+			if (t < 1e-4 || t > 1 - 1e-4) {
+				return null;
+			}
+
+			float u = (abx * ady - aby * adx) / ad_dot_bd;
+			if (u < 1e-4 || u > 1 - 1e-4) {
+				return null;
+			}
+		}
+
+		return new PointF(a.x + t * adx, a.y + t * ady);
 	}
 }

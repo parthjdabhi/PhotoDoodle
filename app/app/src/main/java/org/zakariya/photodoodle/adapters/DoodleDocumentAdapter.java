e4c0d29368580e8b2a21c398e8d0bbe16c846619
Shamyl Zakariya <shamyl@gmail.com>
2015-12-18 08:24:14 -0800

    Make doodle grid items clickable/long-clickable

---
+++ app/app/src/main/java/org/zakariya/photodoodle/adapters/DoodleDocumentAdapter.java
@@ -0,0 +1,173 @@
+package org.zakariya.photodoodle.adapters;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.zakariya.photodoodle.R;
+import org.zakariya.photodoodle.model.DoodleDocument;
+
+import java.lang.ref.WeakReference;
+import java.text.DateFormat;
+import java.util.Date;
+
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
+
+/**
+ * Created by shamyl on 12/18/15.
+ */
+public class DoodleDocumentAdapter extends RecyclerView.Adapter<DoodleDocumentAdapter.ViewHolder> {
+
+	public interface OnClickListener {
+		/**
+		 * Called when an item is clicked
+		 * @param document the document represented by this item
+		 */
+		void onDoodleDocumentClick(DoodleDocument document);
+	}
+
+	public interface OnLongClickListener {
+		/**
+		 * Called when an item is long clicked
+		 * @param document the document represented by this item
+		 * @return true if the long click is handled, false otherwise
+		 */
+		boolean onDoodleDocumentLongClick(DoodleDocument document);
+	}
+
+	public class ViewHolder extends RecyclerView.ViewHolder {
+		public DoodleDocument doodleDocument;
+		public View rootView;
+		public ImageView imageView;
+		public TextView nameTextView;
+		public TextView dateTextView;
+		public TextView uuidTextView;
+
+		public ViewHolder(View v) {
+			super(v);
+			rootView = v;
+			imageView = (ImageView) v.findViewById(R.id.imageView);
+			nameTextView = (TextView) v.findViewById(R.id.nameTextView);
+			dateTextView = (TextView) v.findViewById(R.id.dateTextView);
+			uuidTextView = (TextView) v.findViewById(R.id.uuidTextView);
+		}
+	}
+
+	Context context;
+	View emptyView;
+	RealmResults<DoodleDocument> realmResults;
+	private final RealmChangeListener realmChangeListener;
+	DateFormat dateFormatter;
+	WeakReference<OnClickListener> weakOnClickListener;
+	WeakReference<OnLongClickListener> weakOnLongClickListener;
+
+	public DoodleDocumentAdapter(Context context, RealmResults<DoodleDocument> realmResults, View emptyView) {
+		this.context = context;
+		this.realmResults = realmResults;
+		this.emptyView = emptyView;
+
+		realmChangeListener = new RealmChangeListener() {
+			@Override
+			public void onChange() {
+				updateEmptyView();
+				notifyDataSetChanged();
+			}
+		};
+
+		Realm.getInstance(context).addChangeListener(realmChangeListener);
+
+		dateFormatter = DateFormat.getDateTimeInstance();
+		updateEmptyView();
+	}
+
+	public void setOnClickListener(@Nullable OnClickListener listener) {
+		if (listener != null) {
+			weakOnClickListener = new WeakReference<>(listener);
+		} else {
+			weakOnClickListener = null;
+		}
+	}
+
+	@Nullable
+	public OnClickListener getOnClickListener() {
+		return weakOnClickListener != null ? weakOnClickListener.get() : null;
+	}
+
+	public void setOnLongClickListener(@Nullable OnLongClickListener listener) {
+		if (listener != null) {
+			weakOnLongClickListener = new WeakReference<>(listener);
+		} else {
+			weakOnLongClickListener = null;
+		}
+	}
+
+	@Nullable
+	public OnLongClickListener getOnLongClickListener() {
+		return weakOnLongClickListener != null ? weakOnLongClickListener.get() : null;
+	}
+
+
+	public void onDestroy() {
+		Realm.getInstance(context).removeChangeListener(realmChangeListener);
+	}
+
+	void updateEmptyView() {
+		emptyView.setVisibility(realmResults.isEmpty() ? View.VISIBLE : View.GONE);
+	}
+
+	@Override
+	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+		View v = inflater.inflate(R.layout.doodle_document_grid_item, parent, false);
+
+		final ViewHolder holder = new ViewHolder(v);
+		holder.rootView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				DoodleDocument doc = holder.doodleDocument;
+				OnClickListener listener = getOnClickListener();
+				if (doc != null && listener != null) {
+					listener.onDoodleDocumentClick(doc);
+				}
+			}
+		});
+
+		holder.rootView.setLongClickable(true);
+		holder.rootView.setOnLongClickListener(new View.OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View v) {
+				DoodleDocument doc = holder.doodleDocument;
+				OnLongClickListener listener = getOnLongClickListener();
+				return doc != null && listener != null && listener.onDoodleDocumentLongClick(doc);
+			}
+		});
+
+		return holder;
+	}
+
+	@Override
+	public void onBindViewHolder(ViewHolder holder, int position) {
+
+		DoodleDocument doc = realmResults.get(position);
+		holder.doodleDocument = doc;
+
+		holder.nameTextView.setText(doc.getName());
+
+		Date date = doc.getModificationDate() != null ? doc.getModificationDate() : doc.getCreationDate();
+		holder.dateTextView.setText(dateFormatter.format(date));
+
+		holder.uuidTextView.setText(doc.getUuid());
+	}
+
+	@Override
+	public int getItemCount() {
+		return realmResults.size();
+	}
+}
--- app/app/src/main/java/org/zakariya/photodoodle/fragments/DoodleDocumentGridFragment.java
+++ app/app/src/main/java/org/zakariya/photodoodle/fragments/DoodleDocumentGridFragment.java
@@ -1,6 +1,5 @@
 package org.zakariya.photodoodle.fragments;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,35 +7,33 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import org.zakariya.photodoodle.R;
 import org.zakariya.photodoodle.activities.DoodleActivity;
+import org.zakariya.photodoodle.adapters.DoodleDocumentAdapter;
 import org.zakariya.photodoodle.model.DoodleDocument;
-
-import java.text.DateFormat;
-import java.util.Date;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import icepick.Icepick;
 import io.realm.Realm;
-import io.realm.RealmChangeListener;
 import io.realm.RealmResults;
 import io.realm.Sort;
 
 /**
  * Created by shamyl on 12/16/15.
  */
-public class DoodleDocumentGridFragment extends Fragment {
+public class DoodleDocumentGridFragment extends Fragment implements DoodleDocumentAdapter.OnClickListener, DoodleDocumentAdapter.OnLongClickListener {
+
+	private static final String TAG = DoodleDocumentGridFragment.class.getSimpleName();
 
 	@Bind(R.id.recyclerView)
 	RecyclerView recyclerView;
@@ -93,11 +90,13 @@ public class DoodleDocumentGridFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_doodle_document_grid, container, false);
 		ButterKnife.bind(this, v);
 
-		layoutManager = new GridLayoutManager(getContext(), 3);
+		layoutManager = new GridLayoutManager(getContext(), 2);
 		recyclerView.setLayoutManager(layoutManager);
 
 		RealmResults<DoodleDocument> docs = Realm.getInstance(getContext()).allObjectsSorted(DoodleDocument.class, "modificationDate", Sort.DESCENDING);
 		adapter = new DoodleDocumentAdapter(getContext(), docs, emptyView);
+		adapter.setOnClickListener(this);
+		adapter.setOnLongClickListener(this);
 		recyclerView.setAdapter(adapter);
 
 
@@ -110,84 +109,21 @@ public class DoodleDocumentGridFragment extends Fragment {
 		//editPhotoDoodle(doc);
 	}
 
+	@Override
+	public void onDoodleDocumentClick(DoodleDocument document) {
+		Log.i(TAG, "onDoodleDocumentClick: " + document.getUuid());
+	}
+
+	@Override
+	public boolean onDoodleDocumentLongClick(DoodleDocument document) {
+		Log.i(TAG, "onDoodleDocumentLongClick: " + document.getUuid());
+		return true;
+	}
+
 	public void editPhotoDoodle(DoodleDocument doc) {
 		Intent intent = new Intent(getContext(), DoodleActivity.class);
 
 		startActivity(intent);
 	}
 
-	static class DoodleDocumentAdapter extends RecyclerView.Adapter<DoodleDocumentAdapter.ViewHolder> {
-
-
-		public class ViewHolder extends RecyclerView.ViewHolder {
-			public ImageView imageView;
-			public TextView nameTextView;
-			public TextView dateTextView;
-			public TextView uuidTextView;
-
-			public ViewHolder(View v) {
-				super(v);
-				imageView = (ImageView) v.findViewById(R.id.imageView);
-				nameTextView = (TextView) v.findViewById(R.id.nameTextView);
-				dateTextView = (TextView) v.findViewById(R.id.dateTextView);
-				uuidTextView = (TextView) v.findViewById(R.id.uuidTextView);
-			}
-		}
-
-		Context context;
-		View emptyView;
-		RealmResults<DoodleDocument> realmResults;
-		private final RealmChangeListener realmChangeListener;
-		DateFormat dateFormatter;
-
-		DoodleDocumentAdapter(Context context, RealmResults<DoodleDocument> realmResults, View emptyView) {
-			this.context = context;
-			this.realmResults = realmResults;
-			this.emptyView = emptyView;
-
-			realmChangeListener = new RealmChangeListener() {
-				@Override
-				public void onChange() {
-					updateEmptyView();
-					notifyDataSetChanged();
-				}
-			};
-
-			Realm.getInstance(context).addChangeListener(realmChangeListener);
-
-			dateFormatter = DateFormat.getDateTimeInstance();
-		}
-
-		void onDestroy() {
-			Realm.getInstance(context).removeChangeListener(realmChangeListener);
-		}
-
-		void updateEmptyView(){
-			emptyView.setVisibility(realmResults.isEmpty() ? View.VISIBLE : View.GONE);
-		}
-
-		@Override
-		public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-			LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-			View v = inflater.inflate(R.layout.doodle_document_grid_item, parent, false);
-			return new ViewHolder(v);
-		}
-
-		@Override
-		public void onBindViewHolder(ViewHolder holder, int position) {
-
-			DoodleDocument doc = realmResults.get(position);
-			holder.nameTextView.setText(doc.getName());
-
-			Date date = doc.getModificationDate() != null ? doc.getModificationDate() : doc.getCreationDate();
-			holder.dateTextView.setText(dateFormatter.format(date));
-
-			holder.uuidTextView.setText(doc.getUuid());
-		}
-
-		@Override
-		public int getItemCount() {
-			return realmResults.size();
-		}
-	}
 }

2054ef83835b13c7778addbdcd9811c6d768c96f
Shamyl Zakariya <shamyl@gmail.com>
2016-05-25 08:36:58 -0700

    Draw shadows on the closed and open states of the flout menu

--- PhotoDoodle/app/src/main/java/org/zakariya/mrdoodle/view/FlyoutMenuView.java
+++ PhotoDoodle/app/src/main/java/org/zakariya/mrdoodle/view/FlyoutMenuView.java
@@ -3,12 +3,19 @@ package org.zakariya.mrdoodle.view;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
+import android.graphics.RadialGradient;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Shader;
+import android.support.annotation.ColorInt;
+import android.support.v4.graphics.ColorUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
@@ -20,18 +27,42 @@ import org.zakariya.mrdoodle.R;
 public class FlyoutMenuView extends View implements ValueAnimator.AnimatorUpdateListener {
 
 	private static final String TAG = FlyoutMenuView.class.getSimpleName();
-	private static final int MENU_CORNER_RADIUS_DP = 4;
 
-	Paint menuBackgroundFillPaint;
-	Path menuBackgroundClipPath;
+
+	private static final int MENU_CORNER_RADIUS_DP = 4;
+	private static final int MENU_SHADOW_RADIUS_DP = 24;
+	private static final int MENU_SHADOW_INSET_DP = 16;
+	private static final int MENU_SHADOW_OFFSET_DP = 8;
+
+	private static final int BUTTON_SHADOW_RADIUS_DP = 12;
+	private static final int BUTTON_SHADOW_OFFSET_DP = 5;
+
+	private static final int SHADOW_COLOR = 0x44000000;
+
+
+	Paint menuPaint;
+	Path menuOpenShapePath;
 	float menuBackgroundCornerRadius;
 	int menuBackgroundColor = 0xFFFFFFFF;
 	float menuExtentOpen; // 0 is closed, 1 is menuOpen
 	PointF menuBackgroundFillOrigin;
+	RectF menuClosedRect;
 	RectF menuOpenRect;
 	RectF menuBackgroundFillOval = new RectF();
 	float menuClosedRadius;
 	float menuOpenRadius;
+
+	int menuShadowRadius;
+	int menuShadowInset;
+	int menuShadowOffset;
+	int buttonShadowRadius;
+	Bitmap menuShadowBitmap;
+	Rect menuShadowSrcRect = new Rect();
+	Rect menuShadowDstRect = new Rect();
+
+	Bitmap buttonShadowBitmap;
+	int buttonShadowInset;
+	int buttonShadowOffset;
 
 	public FlyoutMenuView(Context context) {
 		super(context);
@@ -49,8 +80,8 @@ public class FlyoutMenuView extends View implements ValueAnimator.AnimatorUpdate
 	}
 
 	private void init(AttributeSet attrs, int defStyle) {
-		menuBackgroundFillPaint = new Paint();
-		menuBackgroundFillPaint.setAntiAlias(true);
+		menuPaint = new Paint();
+		menuPaint.setAntiAlias(true);
 
 		// Load attributes
 		final TypedArray a = getContext().obtainStyledAttributes(
@@ -59,7 +90,14 @@ public class FlyoutMenuView extends View implements ValueAnimator.AnimatorUpdate
 		setMenuBackgroundColor(a.getColor(R.styleable.FlyoutMenuView_menuColor, menuBackgroundColor));
 		a.recycle();
 
-		menuBackgroundCornerRadius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, MENU_CORNER_RADIUS_DP, getResources().getDisplayMetrics());
+		menuBackgroundCornerRadius = dp2px(MENU_CORNER_RADIUS_DP);
+		menuShadowRadius = (int) dp2px(MENU_SHADOW_RADIUS_DP);
+		menuShadowInset = (int) dp2px(MENU_SHADOW_INSET_DP);
+		menuShadowOffset = (int) dp2px(MENU_SHADOW_OFFSET_DP);
+		menuShadowBitmap = createMenuShadowBitmap(SHADOW_COLOR, menuShadowRadius);
+
+		buttonShadowRadius = (int) dp2px(BUTTON_SHADOW_RADIUS_DP);
+		buttonShadowOffset = (int) dp2px(BUTTON_SHADOW_OFFSET_DP);
 	}
 
 	@Override
@@ -75,14 +113,31 @@ public class FlyoutMenuView extends View implements ValueAnimator.AnimatorUpdate
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
 		updateLayoutInfo();
+		buttonShadowBitmap = createButtonShadowBitmap(SHADOW_COLOR, (int) menuClosedRadius, buttonShadowRadius);
 		invalidate();
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 
+		final float transitionShadowEdgeWeight = 0.1f;
+		final float openMenuShadowEdge = 1 - transitionShadowEdgeWeight;
+
+		if (menuExtentOpen < transitionShadowEdgeWeight) {
+			float buttonShadowAlpha = (transitionShadowEdgeWeight - menuExtentOpen) / transitionShadowEdgeWeight;
+
+			menuPaint.setAlpha((int)(buttonShadowAlpha * 255));
+			canvas.drawBitmap(buttonShadowBitmap, menuBackgroundFillOrigin.x - buttonShadowBitmap.getWidth()/2, menuBackgroundFillOrigin.y - buttonShadowBitmap.getHeight()/2 + buttonShadowOffset, menuPaint);
+		}
+
+		if (menuExtentOpen > openMenuShadowEdge) {
+			float openMenuAlpha = (menuExtentOpen - openMenuShadowEdge) / transitionShadowEdgeWeight;
+			drawMenuShadow(canvas, menuPaint, menuOpenRect, menuShadowBitmap, menuShadowRadius, menuShadowInset, 0, menuShadowOffset, openMenuAlpha);
+		}
+
 		// set clip to the menu shape
-		canvas.clipPath(menuBackgroundClipPath);
+		canvas.save();
+		canvas.clipPath(menuOpenShapePath);
 
 		// fill an oval centered at origin
 		float radius = menuClosedRadius + (menuExtentOpen * (menuOpenRadius - menuClosedRadius));
@@ -90,7 +145,124 @@ public class FlyoutMenuView extends View implements ValueAnimator.AnimatorUpdate
 		menuBackgroundFillOval.top = menuBackgroundFillOrigin.y - radius;
 		menuBackgroundFillOval.right = menuBackgroundFillOrigin.x + radius;
 		menuBackgroundFillOval.bottom = menuBackgroundFillOrigin.y + radius;
-		canvas.drawOval(menuBackgroundFillOval, menuBackgroundFillPaint);
+
+		menuPaint.setAlpha(255);
+		canvas.drawOval(menuBackgroundFillOval, menuPaint);
+		canvas.restore();
+	}
+
+	void drawMenuShadow(Canvas canvas, Paint paint, RectF rect, Bitmap shadowBitmap, int shadowRadius, int inset, int xOffset, int yOffset, float alpha) {
+		canvas.save();
+		paint.setAlpha((int) (alpha * 255));
+
+		int left = (int) rect.left - shadowRadius + inset + xOffset;
+		int top = (int) rect.top - shadowRadius + inset + yOffset;
+		int right = (int) rect.right + shadowRadius - inset + xOffset;
+		int bottom = (int) rect.bottom + shadowRadius - inset + yOffset;
+
+		// top left corner
+		menuShadowSrcRect.left = 0;
+		menuShadowSrcRect.top = 0;
+		menuShadowSrcRect.right = shadowRadius;
+		menuShadowSrcRect.bottom = shadowRadius;
+		menuShadowDstRect.left = left;
+		menuShadowDstRect.top = top;
+		menuShadowDstRect.right = left + shadowRadius;
+		menuShadowDstRect.bottom = top + shadowRadius;
+		canvas.drawBitmap(shadowBitmap, menuShadowSrcRect, menuShadowDstRect, paint);
+
+		// top right corner
+		menuShadowSrcRect.left = shadowBitmap.getWidth() - shadowRadius;
+		menuShadowSrcRect.top = 0;
+		menuShadowSrcRect.right = shadowBitmap.getWidth();
+		menuShadowSrcRect.bottom = shadowRadius;
+		menuShadowDstRect.left = right - shadowRadius;
+		menuShadowDstRect.top = top;
+		menuShadowDstRect.right = right;
+		menuShadowDstRect.bottom = top + shadowRadius;
+		canvas.drawBitmap(shadowBitmap, menuShadowSrcRect, menuShadowDstRect, paint);
+
+		// bottom right corner
+		menuShadowSrcRect.left = shadowBitmap.getWidth() - shadowRadius;
+		menuShadowSrcRect.top = shadowBitmap.getHeight() - shadowRadius;
+		menuShadowSrcRect.right = shadowBitmap.getWidth();
+		menuShadowSrcRect.bottom = shadowBitmap.getHeight();
+		menuShadowDstRect.left = right - shadowRadius;
+		menuShadowDstRect.top = bottom - shadowRadius;
+		menuShadowDstRect.right = right;
+		menuShadowDstRect.bottom = bottom;
+		canvas.drawBitmap(shadowBitmap, menuShadowSrcRect, menuShadowDstRect, paint);
+
+		// bottom left corner
+		menuShadowSrcRect.left = 0;
+		menuShadowSrcRect.top = shadowBitmap.getHeight() - shadowRadius;
+		menuShadowSrcRect.right = shadowRadius;
+		menuShadowSrcRect.bottom = shadowBitmap.getHeight();
+		menuShadowDstRect.left = left;
+		menuShadowDstRect.top = bottom - shadowRadius;
+		menuShadowDstRect.right = left + shadowRadius;
+		menuShadowDstRect.bottom = bottom;
+		canvas.drawBitmap(shadowBitmap, menuShadowSrcRect, menuShadowDstRect, paint);
+
+		// draw the top edge
+		menuShadowSrcRect.left = shadowRadius;
+		menuShadowSrcRect.top = 0;
+		menuShadowSrcRect.right = shadowBitmap.getWidth() - shadowRadius;
+		menuShadowSrcRect.bottom = shadowRadius;
+		menuShadowDstRect.left = left + shadowRadius;
+		menuShadowDstRect.top = top;
+		menuShadowDstRect.right = right - shadowRadius;
+		menuShadowDstRect.bottom = top + shadowRadius;
+		canvas.drawBitmap(shadowBitmap, menuShadowSrcRect, menuShadowDstRect, paint);
+
+		// draw the right edge
+		menuShadowSrcRect.left = shadowBitmap.getWidth() - shadowRadius;
+		menuShadowSrcRect.top = shadowRadius;
+		menuShadowSrcRect.right = shadowBitmap.getWidth();
+		menuShadowSrcRect.bottom = shadowBitmap.getHeight() - shadowRadius;
+		menuShadowDstRect.left = right - shadowRadius;
+		menuShadowDstRect.top = top + shadowRadius;
+		menuShadowDstRect.right = right;
+		menuShadowDstRect.bottom = bottom - shadowRadius;
+		canvas.drawBitmap(shadowBitmap, menuShadowSrcRect, menuShadowDstRect, paint);
+
+		// draw the bottom edge
+		menuShadowSrcRect.left = shadowRadius;
+		menuShadowSrcRect.top = shadowBitmap.getHeight() - shadowRadius;
+		menuShadowSrcRect.right = shadowBitmap.getWidth() - shadowRadius;
+		menuShadowSrcRect.bottom = shadowBitmap.getHeight();
+		menuShadowDstRect.left = left + shadowRadius;
+		menuShadowDstRect.top = bottom - shadowRadius;
+		menuShadowDstRect.right = right - shadowRadius;
+		menuShadowDstRect.bottom = bottom;
+		canvas.drawBitmap(shadowBitmap, menuShadowSrcRect, menuShadowDstRect, paint);
+
+		// draw center
+		menuShadowSrcRect.left = shadowRadius;
+		menuShadowSrcRect.top = shadowRadius;
+		menuShadowSrcRect.right = shadowBitmap.getWidth() - shadowRadius;
+		menuShadowSrcRect.bottom = shadowBitmap.getHeight() - shadowRadius;
+		menuShadowDstRect.left = left + shadowRadius;
+		menuShadowDstRect.top = top + shadowRadius;
+		menuShadowDstRect.right = right - shadowRadius;
+		menuShadowDstRect.bottom = bottom - shadowRadius;
+		canvas.drawBitmap(shadowBitmap, menuShadowSrcRect, menuShadowDstRect, paint);
+
+		// draw the left edge
+		menuShadowSrcRect.left = 0;
+		menuShadowSrcRect.top = shadowRadius;
+		menuShadowSrcRect.right = shadowRadius;
+		menuShadowSrcRect.bottom = shadowBitmap.getHeight() - shadowRadius;
+		menuShadowDstRect.left = left;
+		menuShadowDstRect.top = top + shadowRadius;
+		menuShadowDstRect.right = left + shadowRadius;
+		menuShadowDstRect.bottom = bottom - shadowRadius;
+		canvas.drawBitmap(shadowBitmap, menuShadowSrcRect, menuShadowDstRect, paint);
+
+
+		// restore drawing state
+		canvas.restore();
+		paint.setAlpha(255);
 	}
 
 	@Override
@@ -129,7 +301,7 @@ public class FlyoutMenuView extends View implements ValueAnimator.AnimatorUpdate
 
 	public void setMenuBackgroundColor(int menuBackgroundColor) {
 		this.menuBackgroundColor = menuBackgroundColor;
-		menuBackgroundFillPaint.setColor(this.menuBackgroundColor);
+		menuPaint.setColor(this.menuBackgroundColor);
 		invalidate();
 	}
 
@@ -138,12 +310,17 @@ public class FlyoutMenuView extends View implements ValueAnimator.AnimatorUpdate
 		float innerHeight = getHeight() - getPaddingTop() + getPaddingBottom();
 		menuBackgroundFillOrigin = new PointF(getPaddingLeft() + innerWidth / 2, getPaddingTop() + innerHeight / 2);
 		menuClosedRadius = Math.min(innerWidth / 2, innerHeight / 2);
+		menuClosedRect = new RectF(
+				menuBackgroundFillOrigin.x - menuClosedRadius,
+				menuBackgroundFillOrigin.y - menuClosedRadius,
+				menuBackgroundFillOrigin.x + menuClosedRadius,
+				menuBackgroundFillOrigin.y + menuClosedRadius);
 
 		float openRectWidth = innerWidth * 3;
 		float openRectHeight = innerHeight * 3;
-		menuOpenRect = new RectF(menuBackgroundFillOrigin.x - openRectWidth / 2, menuBackgroundFillOrigin.y - openRectHeight / 2, menuBackgroundFillOrigin.x + openRectWidth/2, menuBackgroundFillOrigin.y + openRectHeight/2);
-		menuBackgroundClipPath = new Path();
-		menuBackgroundClipPath.addRoundRect(menuOpenRect, menuBackgroundCornerRadius, menuBackgroundCornerRadius, Path.Direction.CW);
+		menuOpenRect = new RectF(menuBackgroundFillOrigin.x - openRectWidth / 2, menuBackgroundFillOrigin.y - openRectHeight / 2, menuBackgroundFillOrigin.x + openRectWidth / 2, menuBackgroundFillOrigin.y + openRectHeight / 2);
+		menuOpenShapePath = new Path();
+		menuOpenShapePath.addRoundRect(menuOpenRect, menuBackgroundCornerRadius, menuBackgroundCornerRadius, Path.Direction.CW);
 
 		// compute the circular radius to fill the menuOpenRect
 		float a = distToOrigin(menuOpenRect.left, menuOpenRect.top);
@@ -156,4 +333,47 @@ public class FlyoutMenuView extends View implements ValueAnimator.AnimatorUpdate
 	float distToOrigin(float x, float y) {
 		return (float) Math.sqrt((x - menuBackgroundFillOrigin.x) * (x - menuBackgroundFillOrigin.x) + (y - menuBackgroundFillOrigin.y) * (y - menuBackgroundFillOrigin.y));
 	}
+
+	Bitmap createMenuShadowBitmap(@ColorInt int color, int shadowRadiusPx) {
+		int size = 2 * shadowRadiusPx + 1;
+		Bitmap shadowBitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+		shadowBitmap.eraseColor(0x0); // clear
+
+		Canvas canvas = new Canvas(shadowBitmap);
+		Paint paint = new Paint();
+		paint.setAntiAlias(true);
+		paint.setShader(new RadialGradient(shadowRadiusPx + 1, shadowRadiusPx + 1, shadowRadiusPx, color, ColorUtils.setAlphaComponent(color, 0), Shader.TileMode.CLAMP));
+
+		canvas.drawRect(0, 0, size, size, paint);
+
+		Log.i(TAG, "createMenuShadowBitmap: rendered bitmap, width: " + shadowBitmap.getWidth() + " height: " + shadowBitmap.getHeight());
+
+		return shadowBitmap;
+	}
+
+	Bitmap createButtonShadowBitmap(@ColorInt int color, int buttonRadius, int shadowRadius) {
+		int radius = buttonRadius + shadowRadius / 2;
+		int size = radius * 2;
+		Bitmap shadowBitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+		shadowBitmap.eraseColor(0x0);
+
+		Canvas canvas = new Canvas(shadowBitmap);
+		Paint paint = new Paint();
+		paint.setAntiAlias(true);
+
+		int colors[] = {color, ColorUtils.setAlphaComponent(color, 0)};
+		float stops[] = {(float) (buttonRadius - (shadowRadius / 2)) / (float) radius, 1f};
+		paint.setShader(new RadialGradient(radius, radius, radius, colors, stops, Shader.TileMode.CLAMP));
+
+		canvas.drawRect(0, 0, size, size, paint);
+
+		Log.i(TAG, "createButtonShadowBitmap: rendered bitmap, width: " + shadowBitmap.getWidth() + " height: " + shadowBitmap.getHeight());
+
+		return shadowBitmap;
+	}
+
+	float dp2px(float dp) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
+	}
+
 }
